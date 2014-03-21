5c294ae886fe0c15c4c43f174c80dc1a61e131bd
Xinran Wang <xw1992@gmail.com>
2014-03-21 14:58:02 -0400

    Initial setup.

---
+++ NursingDocumentTracker/XIWAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  XIWAppDelegate.h
+//  NursingDocumentTracker
+//
+//  Created by User on 3/21/14.
+//  Copyright (c) 2014 CIS350. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XIWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ NursingDocumentTracker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NursingDocumentTracker
+//
+//  Created by User on 3/21/14.
+//  Copyright (c) 2014 CIS350. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XIWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XIWAppDelegate class]));
+    }
+}
